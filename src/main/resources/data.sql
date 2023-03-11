c1267a308d520c686a657249348d0dbad6014613
jinsk9268 <jinsk9268@gmail.com>
2023-03-12 01:15:56 +0900

    feat: DB 초기 데이터 삽입 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into account_user(id, name, created_at, updated_at)
+values (1, '진조이',  now(), now());
+insert into account_user(id, name, created_at, updated_at)
+values (2, '진',  now(), now());
+insert into account_user(id, name, created_at, updated_at)
+values (3, '조이',  now(), now());
