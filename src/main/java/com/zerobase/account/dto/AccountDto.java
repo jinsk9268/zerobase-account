15b7977338759bb17a26673aa54c27cd08c74ef4
jinsk9268 <jinsk9268@gmail.com>
2023-03-12 00:34:29 +0900

    feat: AccountDto 생성

---
+++ src/main/java/com/zerobase/account/dto/AccountDto.java
@@ -0,0 +1,19 @@
+package com.zerobase.account.dto;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AccountDto {
+    private Long userId;
+    private String accountNumber;
+    private Long balance;
+    private LocalDateTime registeredAt;
+    private LocalDateTime unRegisteredAt;
+
+}
