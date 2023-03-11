595f3b4d2a6fa790a49c5e946993ec3561d46eec
jinsk9268 <jinsk9268@gmail.com>
2023-03-12 00:06:14 +0900

    feat: AccountUser 테이블(클래스) 추가

---
+++ src/main/java/com/zerobase/account/domain/AccountUser.java
@@ -0,0 +1,31 @@
+package com.zerobase.account.domain;
+
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class AccountUser {
+    @Id
+    @Generated
+    private Long id;
+
+    private String name;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
