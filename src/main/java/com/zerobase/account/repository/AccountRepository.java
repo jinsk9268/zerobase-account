45eaa1812aa442c025ea0f51be6109fd552e3060
jinsk9268 <jinsk9268@gmail.com>
2023-03-12 00:07:53 +0900

    feat: AccountRepository 추가

---
+++ src/main/java/com/zerobase/account/repository/AccountRepository.java
@@ -0,0 +1,10 @@
+package com.zerobase.account.repository;
+
+import com.zerobase.account.domain.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface AccountRepository extends JpaRepository<Account, Long> {
+    Optional<Account> findFirstByOrderByIdDesc();
+}
