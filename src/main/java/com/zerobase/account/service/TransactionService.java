ef2a1784f2e10f72079785beef51732a6a888c93
jinsk9268 <jinsk9268@gmail.com>
2023-03-13 03:19:00 +0900

    feat: TransactionService 잔액 사용 method 초기 세팅
    - useBalance method

---
+++ src/main/java/com/zerobase/account/service/TransactionService.java
@@ -0,0 +1,39 @@
+package com.zerobase.account.service;
+
+import com.zerobase.account.domain.Account;
+import com.zerobase.account.domain.AccountUser;
+import com.zerobase.account.dto.TransactionDto;
+import com.zerobase.account.exception.AccountException;
+import com.zerobase.account.repository.AccountRepository;
+import com.zerobase.account.repository.AccountUserRepository;
+import com.zerobase.account.repository.TransactionRepository;
+import com.zerobase.account.type.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+
+/**
+ * 잔액 사용 API 정책
+ * 사용자 없는 경우, 계좌가 없는 경우, 사용자 아이디와 계좌 소유주가 다른 경우,
+ * 계좌가 이미 해지 상태인 경우, 거래금액이 잔액보다 큰 경우,
+ * 거래금액이 너무 작거나 큰 경우 실패 응답
+ */
+public class TransactionService {
+    private final TransactionRepository transactionRepository;
+    private final AccountUserRepository accountUserRepository;
+    private final AccountRepository accountRepository;
+
+    @Transactional
+    public TransactionDto useBalance(Long userId, String accountNumber, Long amount) {
+        AccountUser user = accountUserRepository.findById(userId)
+                .orElseThrow(() -> new AccountException(ErrorCode.USER_NOT_FOUND));
+        Account account = accountRepository.findByAccountNumber(accountNumber)
+                .orElseThrow(() -> new AccountException(ErrorCode.ACCOUNT_NOT_FOUND));
+    }
+}
