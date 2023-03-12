ef907714f6b7fe8006b279ff171118e38fc794cc
jinsk9268 <jinsk9268@gmail.com>
2023-03-13 03:06:39 +0900

    feat: TransactionDto 생성

---
+++ src/main/java/com/zerobase/account/dto/TransactionDto.java
@@ -0,0 +1,23 @@
+package com.zerobase.account.dto;
+
+import com.zerobase.account.type.TransactionResultType;
+import com.zerobase.account.type.TransactionType;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TransactionDto {
+    private String accountNumber;
+    private TransactionType transactionType;
+    private TransactionResultType transactionResultType;
+    private Long amount;
+    private Long balanceSnapshot;
+    private String transactionId;
+    private LocalDateTime transactedAt;
+
+}
