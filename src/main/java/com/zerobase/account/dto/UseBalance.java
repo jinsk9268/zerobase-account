d36fd22758b660055dc8fd59e38ce2130b8b0f05
jinsk9268 <jinsk9268@gmail.com>
2023-03-13 02:45:05 +0900

    feat: UseBalance dto Response 생성

--- src/main/java/com/zerobase/account/dto/UseBalance.java
+++ src/main/java/com/zerobase/account/dto/UseBalance.java
@@ -1,10 +1,10 @@
 package com.zerobase.account.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
+import com.zerobase.account.type.TransactionResultType;
+import lombok.*;
 
 import javax.validation.constraints.*;
+import java.time.LocalDateTime;
 
 public class UseBalance {
     /**
@@ -29,5 +29,26 @@ public class UseBalance {
         @Min(10)
         @Max(1_000_000_000)
         private Long amount;
+    }
+
+    /**
+     * 응답 값
+     * "accountNumber":"1234567890",
+     * "transactionResult":"S",
+     * "transactionId":"c2033bb6d82a4250aecf8e27c49b63f6",
+     * "amount":1000,
+     * "transactedAt":"2022-06-01T23:26:14.671859"
+     */
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class Response {
+        private String accountNumber;
+        private TransactionResultType transactionResult;
+        private String transactionId;
+        private Long amount;
+        private LocalDateTime transactedAt;
     }
 }
