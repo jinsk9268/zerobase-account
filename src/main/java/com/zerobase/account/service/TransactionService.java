4dcca57307c3f56d3de07e7fe8c0c1bcb32c2284
jinsk9268 <jinsk9268@gmail.com>
2023-03-13 03:31:09 +0900

    feat: TransactionService 잔액 사용 검증 method 추가
    - validateUseBalance

--- src/main/java/com/zerobase/account/service/TransactionService.java
+++ src/main/java/com/zerobase/account/service/TransactionService.java
@@ -7,23 +7,19 @@ import com.zerobase.account.exception.AccountException;
 import com.zerobase.account.repository.AccountRepository;
 import com.zerobase.account.repository.AccountUserRepository;
 import com.zerobase.account.repository.TransactionRepository;
+import com.zerobase.account.type.AccountStatus;
 import com.zerobase.account.type.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.Objects;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 
-/**
- * 잔액 사용 API 정책
- * 사용자 없는 경우, 계좌가 없는 경우, 사용자 아이디와 계좌 소유주가 다른 경우,
- * 계좌가 이미 해지 상태인 경우, 거래금액이 잔액보다 큰 경우,
- * 거래금액이 너무 작거나 큰 경우 실패 응답
- */
 public class TransactionService {
     private final TransactionRepository transactionRepository;
     private final AccountUserRepository accountUserRepository;
@@ -35,5 +31,27 @@ public class TransactionService {
                 .orElseThrow(() -> new AccountException(ErrorCode.USER_NOT_FOUND));
         Account account = accountRepository.findByAccountNumber(accountNumber)
                 .orElseThrow(() -> new AccountException(ErrorCode.ACCOUNT_NOT_FOUND));
+
+        validateUseBalance(user, account, amount);
+    }
+
+    /**
+     * 잔액 사용 API 정책
+     * 사용자 없는 경우, 계좌가 없는 경우, 사용자 아이디와 계좌 소유주가 다른 경우,
+     * 계좌가 이미 해지 상태인 경우, 거래금액이 잔액보다 큰 경우,
+     * 거래금액이 너무 작거나 큰 경우 실패 응답 (-> Request Body의 validate으로 진행)
+     */
+    private void validateUseBalance(AccountUser user, Account account, Long amount) {
+        if (!Objects.equals(user.getId(), account.getAccountUser().getId())) {
+            throw new AccountException(ErrorCode.USER_ACCOUNT_UN_MATCH);
+        }
+
+        if (account.getAccountStatus() != AccountStatus.IN_USE) {
+            throw new AccountException(ErrorCode.ACCOUNT_ALREADY_UNREGISTERED);
+        }
+
+        if (account.getBalance() < amount) {
+            throw new AccountException(ErrorCode.AMOUNT_EXCEED_BALANCE);
+        }
     }
 }
