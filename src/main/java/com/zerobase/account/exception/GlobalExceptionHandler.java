a42d04927b9a0f28cf55ed85ce1d43367b9d5a72
jinsk9268 <jinsk9268@gmail.com>
2023-03-13 21:06:26 +0900

    feat: 모든 Exception에 대한 에러 헨들러 추가
    - handleException

--- src/main/java/com/zerobase/account/exception/GlobalExceptionHandler.java
+++ src/main/java/com/zerobase/account/exception/GlobalExceptionHandler.java
@@ -5,6 +5,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import static com.zerobase.account.type.ErrorCode.INTERNAL_SERVER_ERROR;
+
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -14,5 +16,15 @@ public class GlobalExceptionHandler {
         log.error("{} is occurred.", e.getErrorCode());
 
         return new ErrorResponse(e.getErrorCode(), e.getErrorMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ErrorResponse handleException(Exception e) {
+        log.error("Exception is occurred.", e);
+
+        return new ErrorResponse(
+                INTERNAL_SERVER_ERROR,
+                INTERNAL_SERVER_ERROR.getDescription()
+        );
     }
 }
