34be5e761e3e9e2d0b442b8a96ca3841c6771f5d
jinsk9268 <jinsk9268@gmail.com>
2023-03-13 22:57:57 +0900

    test: LockService 계좌 없음 애러를 던질때도 lock, unlock 실행 유닛 테스트 추가

--- src/test/java/com/zerobase/account/service/LockServiceTest.java
+++ src/test/java/com/zerobase/account/service/LockServiceTest.java
@@ -1,6 +1,8 @@
 package com.zerobase.account.service;
 
 import com.zerobase.account.dto.UseBalance;
+import com.zerobase.account.exception.AccountException;
+import com.zerobase.account.type.ErrorCode;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -10,6 +12,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
@@ -46,4 +49,28 @@ class LockServiceTest {
         assertEquals("0123456789", unlockArgumentCaptor.getValue());
     }
 
+    @Test
+    void lockAndUnlock_EvenIfThrow() throws Throwable {
+        // given
+        ArgumentCaptor<String> lockArgumentCaptor =
+                ArgumentCaptor.forClass(String.class);
+        ArgumentCaptor<String> unlockArgumentCaptor =
+                ArgumentCaptor.forClass(String.class);
+        UseBalance.Request request =
+                new UseBalance.Request(123L, "1234567890", 1000L);
+        given(proceedingJoinPoint.proceed())
+                .willThrow(new AccountException(ErrorCode.ACCOUNT_NOT_FOUND));
+
+        // when
+        assertThrows(AccountException.class,
+                () -> lockAopAspect.aroundMethod(proceedingJoinPoint, request));
+
+        // then
+        verify(lockService, times(1))
+                .lock(lockArgumentCaptor.capture());
+        verify(lockService, times(1))
+                .unlock(unlockArgumentCaptor.capture());
+        assertEquals("1234567890", lockArgumentCaptor.getValue());
+        assertEquals("1234567890", unlockArgumentCaptor.getValue());
+    }
 }
