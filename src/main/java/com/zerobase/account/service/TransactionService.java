1a47a24ea059ec9d0ebb9b15e47c20e47b25c2b3
jinsk9268 <jinsk9268@gmail.com>
2023-03-13 17:05:34 +0900

    feat: 잔액 사용 취소 validate method 추가

--- src/main/java/com/zerobase/account/service/TransactionService.java
+++ src/main/java/com/zerobase/account/service/TransactionService.java
@@ -97,5 +97,23 @@ public class TransactionService {
                 .orElseThrow(() -> new AccountException(ErrorCode.TRANSACTION_NOT_FOUND));
         Account account = accountRepository.findByAccountNumber(accountNumber)
                 .orElseThrow(() -> new AccountException(ErrorCode.ACCOUNT_NOT_FOUND));
+
+        validateCancelBalance(transaction, account, amount);
+    }
+
+    private void validateCancelBalance(
+            Transaction transaction, Account account, Long amount
+    ) {
+        if (!Objects.equals(transaction.getAccount().getId(), account.getId())) {
+            throw new AccountException(ErrorCode.TRANSACTION_ACCOUNT_UN_MATCH);
+        }
+
+        if (!Objects.equals(transaction.getAmount(), amount)) {
+            throw new AccountException(ErrorCode.CANCEL_MUST_FULLY);
+        }
+
+        if (transaction.getTransactedAt().isBefore(LocalDateTime.now().minusYears(1))) {
+            throw new AccountException(ErrorCode.TOO_OLD_ORDER_TO_CANCEL);
+        }
     }
 }
