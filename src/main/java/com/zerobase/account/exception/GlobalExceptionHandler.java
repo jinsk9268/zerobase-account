bb3b1a67fffb71838dedb635e89adcda57218109
jinsk9268 <jinsk9268@gmail.com>
2023-03-13 21:09:00 +0900

    feat: DB 유닉키 중복 저장 등에 대한 에러 헨들러 추가
    - handleDataIntegrityViolationException

--- src/main/java/com/zerobase/account/exception/GlobalExceptionHandler.java
+++ src/main/java/com/zerobase/account/exception/GlobalExceptionHandler.java
@@ -2,10 +2,12 @@ package com.zerobase.account.exception;
 
 import com.zerobase.account.dto.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import static com.zerobase.account.type.ErrorCode.INTERNAL_SERVER_ERROR;
+import static com.zerobase.account.type.ErrorCode.INVALID_REQUEST;
 
 @Slf4j
 @RestControllerAdvice
@@ -16,6 +18,16 @@ public class GlobalExceptionHandler {
         log.error("{} is occurred.", e.getErrorCode());
 
         return new ErrorResponse(e.getErrorCode(), e.getErrorMessage());
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ErrorResponse handleDataIntegrityViolationException(DataIntegrityViolationException e) {
+        log.error("Exception is occurred.", e);
+
+        return new ErrorResponse(
+                INVALID_REQUEST,
+                INVALID_REQUEST.getDescription()
+        );
     }
 
     @ExceptionHandler(Exception.class)
