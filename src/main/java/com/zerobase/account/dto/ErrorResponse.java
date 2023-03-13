0d31ae863eedb29711b0806cb8bec93894cd4486
jinsk9268 <jinsk9268@gmail.com>
2023-03-13 20:57:07 +0900

    feat: ErrorResponse dto 생성
    - 일관성 있는 에러처리를 위해 추가

---
+++ src/main/java/com/zerobase/account/dto/ErrorResponse.java
@@ -0,0 +1,14 @@
+package com.zerobase.account.dto;
+
+import com.zerobase.account.type.ErrorCode;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ErrorResponse {
+    private ErrorCode errorCode;
+    private String errorMessage;
+}
