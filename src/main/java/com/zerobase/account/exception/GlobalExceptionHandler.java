95627c4172d493c903ea4cb90bd3b1fd0d86d130
jinsk9268 <jinsk9268@gmail.com>
2023-03-13 21:01:15 +0900

    feat: AccountException 글로벌 에러 핸들러 추가

---
+++ src/main/java/com/zerobase/account/exception/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.zerobase.account.exception;
+
+import com.zerobase.account.dto.ErrorResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(AccountException.class)
+    public ErrorResponse handleAccountException(AccountException e) {
+        log.error("{} is occurred.", e.getErrorCode());
+
+        return new ErrorResponse(e.getErrorCode(), e.getErrorMessage());
+    }
+}
