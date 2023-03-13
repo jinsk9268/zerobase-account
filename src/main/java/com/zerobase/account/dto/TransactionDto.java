d7301e39934b59409696904f2c26ceca1edbec33
jinsk9268 <jinsk9268@gmail.com>
2023-03-13 10:20:39 +0900

    feat: TransactionDto에 생성자 static method 추가

--- src/main/java/com/zerobase/account/dto/TransactionDto.java
+++ src/main/java/com/zerobase/account/dto/TransactionDto.java
@@ -1,5 +1,6 @@
 package com.zerobase.account.dto;
 
+import com.zerobase.account.domain.Transaction;
 import com.zerobase.account.type.TransactionResultType;
 import com.zerobase.account.type.TransactionType;
 import lombok.*;
@@ -20,4 +21,15 @@ public class TransactionDto {
     private String transactionId;
     private LocalDateTime transactedAt;
 
+    public static TransactionDto fromEntity(Transaction transaction) {
+        return TransactionDto.builder()
+                .accountNumber(transaction.getAccount().getAccountNumber())
+                .transactionType(transaction.getTransactionType())
+                .transactionResultType(transaction.getTransactionResultType())
+                .amount(transaction.getAmount())
+                .balanceSnapshot(transaction.getBalanceSnapshot())
+                .transactionId(transaction.getTransactionId())
+                .transactedAt(transaction.getTransactedAt())
+                .build();
+    }
 }
