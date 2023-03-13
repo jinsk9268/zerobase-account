ae23a4b61e70603d926064f66f0a28328d8646ba
jinsk9268 <jinsk9268@gmail.com>
2023-03-13 14:57:38 +0900

    test: TransactionService 잔액 사용 성공 유닛 테스트 케이스 추가
    - successUseBalance

--- src/test/java/com/zerobase/account/service/TransactionServiceTest.java
+++ src/test/java/com/zerobase/account/service/TransactionServiceTest.java
@@ -1,12 +1,28 @@
 package com.zerobase.account.service;
 
+import com.zerobase.account.domain.Account;
+import com.zerobase.account.domain.AccountUser;
+import com.zerobase.account.domain.Transaction;
+import com.zerobase.account.dto.TransactionDto;
 import com.zerobase.account.repository.AccountRepository;
 import com.zerobase.account.repository.AccountUserRepository;
 import com.zerobase.account.repository.TransactionRepository;
+import com.zerobase.account.type.AccountStatus;
+import com.zerobase.account.type.TransactionResultType;
+import com.zerobase.account.type.TransactionType;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.BDDMockito.given;
+
 
 @ExtendWith(MockitoExtension.class)
 class TransactionServiceTest {
@@ -22,5 +38,41 @@ class TransactionServiceTest {
     @InjectMocks
     private TransactionService transactionService;
 
+    @Test
+    void successUseBalance() {
+        // given
+        AccountUser user = AccountUser.builder()
+                .id(12L).name("jin").build();
+        Account account = Account.builder()
+                .accountUser(user)
+                .accountStatus(AccountStatus.IN_USE)
+                .balance(10000L)
+                .accountNumber("1234567890")
+                .build();
+        given(accountUserRepository.findById(anyLong()))
+                .willReturn(Optional.of(user));
+        given(accountRepository.findByAccountNumber(anyString()))
+                .willReturn(Optional.of(account));
+        given(transactionRepository.save(any()))
+                .willReturn(Transaction.builder()
+                        .account(account)
+                        .transactionType(TransactionType.USE)
+                        .transactionResultType(TransactionResultType.S)
+                        .transactionId("transactionId")
+                        .transactedAt(LocalDateTime.now())
+                        .amount(1000L)
+                        .balanceSnapshot(9000L)
+                        .build());
 
+        // when
+        TransactionDto transactionDto = transactionService.useBalance(
+                1L, "01234567890", 1000L
+        );
+
+        // then
+        assertEquals(TransactionResultType.S, transactionDto.getTransactionResultType());
+        assertEquals(TransactionType.USE, transactionDto.getTransactionType());
+        assertEquals(9000L, transactionDto.getBalanceSnapshot());
+        assertEquals(1000L, transactionDto.getAmount());
+    }
 }
