aa6890a676e03b3272212a577e550c2064106ebd
jinsk9268 <jinsk9268@gmail.com>
2023-03-13 23:02:14 +0900

    fix: 테스트 종류 오류 정정
    - LockServiceTest -> LockAopAspectTest

--- src/test/java/com/zerobase/account/service/LockAopAspectTest.java
+++ src/test/java/com/zerobase/account/service/LockAopAspectTest.java
@@ -17,7 +17,7 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
-class LockServiceTest {
+class LockAopAspectTest {
     @Mock
     private LockService lockService;
 
